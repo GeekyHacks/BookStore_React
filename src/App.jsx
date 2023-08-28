a3f1398011c6c59db0bb1e1c14de6864898794b0
GeekyHacks <geekyhacks22@gmail.com>
2023-08-28 17:25:22 +0300

    Add Redux to the project

--- src/App.jsx
+++ src/App.jsx
@@ -6,40 +6,39 @@ import Categories from './components/Categories';
 import Footer from './components/Footer';
 import './styles/Css/App.css';
 import accountPic from './assets/images/account-circle.svg';
+import Counter from './features/counter/Counter';
 
 function App() {
   return (
-    <div className="fullWebsite">
-      <div className="App">
-        <nav className="navBar">
-          <Link className="bookstoreTitle" to="/BookStore_React/">
-            Geeky Book Store
+    <div className="App">
+      <nav className="navBar">
+        <Link className="bookstoreTitle" to="/BookStore_React/">
+          Geeky Book Store
+        </Link>
+        <div className="navLinks">
+          <Link className="link" to="/BookStore_React/books">
+            Books
           </Link>
-          <div className="navLinks">
-            <Link className="link" to="/BookStore_React/books">
-              Books
-            </Link>
-            <Link className="link" to="/BookStore_React/categories">
-              Categories
-            </Link>
-            <Link className="link" to="/BookStore_React/contact">
-              Contact
-            </Link>
-            <Link className="bookstoreTitle" to="/BookStore_React/">
-              <img className="accountPic" src={accountPic} alt="" />
-            </Link>
-          </div>
-        </nav>
-
-        <Routes>
-          {/* <Route index element={<Home />} /> */}
-          <Route path="/BookStore_React/" element={<MainContainer />} />
-          <Route path="/BookStore_React/books" element={<MainContainer />} />
-          <Route path="/BookStore_React/contact" element={<Contact />} />
-          <Route path="/BookStore_React/categories" element={<Categories />} />
-          <Route path="/BookStore_React/*" element={<div className="notFound"> Page not found!! </div>} />
-        </Routes>
-      </div>
+          <Link className="link" to="/BookStore_React/categories">
+            Categories
+          </Link>
+          <Link className="link" to="/BookStore_React/contact">
+            Contact
+          </Link>
+          <Link className="bookstoreTitle" to="/BookStore_React/">
+            <img className="accountPic" src={accountPic} alt="" />
+          </Link>
+        </div>
+      </nav>
+      <Routes className="Routes">
+        {/* <Route index element={<Home />} /> */}
+        <Route path="/BookStore_React/" element={<MainContainer />} />
+        <Route path="/BookStore_React/books" element={<MainContainer />} />
+        <Route path="/BookStore_React/contact" element={<Contact />} />
+        <Route path="/BookStore_React/categories" element={<Categories />} />
+        {/* <Route path="/BookStore_React/*" element={<div className="notFound"> Page not found!! </div>} /> */}
+        <Route path="/BookStore_React/*" element={<Counter />} />
+      </Routes>
       <Footer />
     </div>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,11 @@
 const Footer = () => {
   const result = (
-    <div>
-      <footer>
-        <h3 className="copyRights">
-          Copy rights reserved to
-          <a href="https://github.com/GeekyHacks"> GeekyHacks</a>
-        </h3>
-      </footer>
-    </div>
+    <footer>
+      <h3 className="copyRights">
+        Copy rights reserved to
+        <a href="https://github.com/GeekyHacks"> GeekyHacks</a>
+      </h3>
+    </footer>
   );
 
   return result;
